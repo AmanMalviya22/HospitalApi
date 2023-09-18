3e0b65f10c3008b38f686397cbc088e7b2a38100
Aman Malviya <amanaman26802@gmail.com>
2023-09-19 00:23:51 +0530

    updated

--- config/authenticate_doctor.js
+++ config/authenticate_doctor.js
@@ -1,17 +1,32 @@
 const Doctor = require("../models/doctors")
 const jwt = require('jsonwebtoken');
 
-// method for setting up authentication of doctors
-module.exports.checkAuthentication =  (req, res, next) => {
 
-    const data = req.headers.authorization; // receives data from header
-    const token= data.split(' ')[1]; // spliting token
-    const decoded = jwt.verify(token , 'secret');// decoding token and varifying
-     
-    if(!decoded) {
-        return res.status(401).json({ 
-            success:false,
-            message: 'TOken expires! please regenerate it!'
+
+
+
+
+
+
+
+
+module.exports.checkAuthentication = (req, res, next) => {
+    const tokenHeader = req.headers.authorization;
+
+    if (!tokenHeader) {
+        return res.status(401).json({
+            success: false,
+            message: 'Authorization header missing!'
+        });
+    }
+
+    const token = tokenHeader.split(' ')[1]; // Split the token
+    const decoded = jwt.verify(token, 'secret'); // Decode and verify the token
+
+    if (!decoded) {
+        return res.status(401).json({
+            success: false,
+            message: 'Token has expired or is invalid. Please regenerate it!'
         });
     }
     next();
--- controllers/api/v1/patient_controller.js
+++ controllers/api/v1/patient_controller.js
@@ -1,86 +1,81 @@
 const Patient = require("../../../models/patients");
 const Report = require("../../../models/reports");
-const jwt = require('../../../config/authenticate_doctor')
+const jwt = require('jsonwebtoken');
 const Doctor = require('../../../models/doctors');
-const { status } = require("./report_controller");
 
-//Register a patient
+// Register a patient
 module.exports.registerPatient = async (req, res) => {
+    try {
+        // Check if a patient with the same phone number exists
+        let existingPatient = await Patient.findOne({ phone: req.body.phone });
 
-try {
+        if (existingPatient) {
+            return res.status(409).json({
+                success: false,
+                message: 'Patient with the same phone number already exists.',
+            });
+        } else {
+            // If the patient doesn't exist, register a new patient
+            const newPatient = new Patient({
+                name: req.body.name,
+                phone: req.body.phone,
+            });
 
-    // check if patient with same phone exists or not
-    let checkpatient = await Patient.find({ phone: req.body.phone });
+            const savedPatient = await newPatient.save();
 
-    // if exists then .find({}) returns array of elements
-    if (checkpatient && checkpatient.length > 0) {
-        return res.status(200).json({
-            success: true,
-            body: checkpatient,
-            message: 'All reports of patient!'
-        });
+            // Generate a JWT token for the newly registered patient
+            const token = jwt.sign({ patientId: savedPatient._id }, 'secret');
 
-    } else {
-
-        // if not exists then registering new patient  
-        checkpatient = new Patient({
-            name: req.body.name,
-            phone: req.body.phone
-        });
-
-        let addPatient = await checkpatient.save(); // saving registered patient in DB
-
-        let pat = jwt.sign(addPatient, id);
-        return res.status(200).json({
-            success:true,
-            body: addPatient,
-            message: 'Patient registerd successfuly!'
+            return res.status(201).json({
+                success: true,
+                token: token,
+                message: 'Patient registered successfully!',
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error!',
         });
     }
-} catch (error) {
-    return res.status(500).json({
-        success:false,
-        message: 'Internal server error!'
-    })
- }
 };
 
-//create patient report
+// Create a patient report
 module.exports.createReport = async (req, res) => {
-
-try {
-
-    // bvefore creating report firstly find all reports of patient 
-    let report = await Report.find(req.body);
-
-    // since .find({}) returns array
-    if (report && report.length > 0) {
-        return res.status(200).json({
-            success: true,
-            body: report,
-            message: 'report already exist!'
-        });
-    } else {
-
-        // if no reports present creating report
-        report = new Report({
-            createdBy: req.body.createdBy,
+    try {
+        // Check if a report for the patient already exists
+        const existingReport = await Report.findOne({
             patientId: req.params.id,
-            status: req.body.status
+            createdBy: req.body.createdBy,
         });
-        let reportCreated = await report.save();// saving report in DB
-        
-        return res.status(200).json({
-            success: true,
-            body: reportCreated,
-            message: 'Report created Successfuly!'
+
+        if (existingReport) {
+            return res.status(409).json({
+                success: false,
+                message: 'Report already exists for this patient.',
+            });
+        } else {
+            // If the report doesn't exist, create a new report
+            const newReport = new Report({
+                createdBy: req.body.createdBy,
+                patientId: req.params.id,
+                status: req.body.status,
+            });
+
+            const savedReport = await newReport.save();
+
+            return res.status(201).json({
+                success: true,
+                body: savedReport,
+                message: 'Report created successfully!',
+            });
+        }
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({
+            success: false,
+            message: 'Internal server error!',
         });
     }
-} catch (error) {
-    console.log(error)
-    return res.status(200).json({
-        success: false,
-        message: 'Internal server error!'
-    });
-  }
 };
